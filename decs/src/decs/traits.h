cca5131f3928ec3049e305fd88a9c5cb307cb45a
Michał <jezierski.michal13@gmail.com>
2023-08-05 16:34:08 +0200

    Added tag traits

--- decs/src/decs/traits.h
+++ decs/src/decs/traits.h
@@ -8,24 +8,48 @@ namespace decs
 	template<typename T>
 	struct component_type
 	{
+	public:
 		using Type = T;
 	};
 
 	template<typename T>
 	struct component_type<stable<T>>
 	{
+	public:
 		using Type = T;
 	};
 
 	template<typename T>
 	struct is_stable
 	{
+	public:
 		static constexpr bool value = false;
 	};
 
 	template<typename T>
 	struct is_stable<stable<T>>
 	{
+	public:
 		static constexpr bool value = true;
 	};
+
+	template<typename T>
+	struct tag
+	{
+	};
+
+	template<typename T>
+	struct is_tag
+	{
+	public:
+		static constexpr bool value = false;
+	};
+
+	template<typename T>
+	struct is_tag<tag<T>>
+	{
+	public:
+		static constexpr bool value = true;
+	};
+
 }
