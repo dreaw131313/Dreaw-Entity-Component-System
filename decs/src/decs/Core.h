7227fcff00a5778bf4adc51c6cf8a1ae1fbbc4ee
Michał <jezierski.michal13@gmail.com>
2024-02-08 21:54:55 +0100

    Removed skarupke hash maps.

--- decs/src/decs/Core.h
+++ decs/src/decs/Core.h
@@ -9,10 +9,6 @@
 #include <string>
 #include <typeinfo>
 
-#include "ThirdParty/skarupke/flat_hash_map.hpp"
-#include "ThirdParty/skarupke/bytell_hash_map.hpp"
-#include "ThirdParty/skarupke/unordered_map.hpp"
-
 #include "traits.h"
 
 namespace decs
@@ -40,15 +36,9 @@ className(className&&) = delete;			\
 className& operator=(className&&) = delete;	\
 
 	template<typename Key, typename Value>
-	//using ecsMap = ska::bytell_hash_map<Key, Value>;
-	//using ecsMap = ska::unordered_map<Key, Value>;
-	//using ecsMap = ska::flat_hash_map<Key, Value>;
 	using ecsMap = std::unordered_map<Key, Value>;
 
 	template<typename Key>
-	//using ecsSet = ska::bytell_hash_set<Key>;
-	//using ecsSet = std::unordered_set<Key>;
-	//using ecsSet = ska::flat_hash_set<Key>;
 	using ecsSet = std::unordered_set<Key>;
 
 	using EntityID = uint32_t;
