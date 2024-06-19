3d34873c804dfdaf02a909a29f8701875bcc32e1
Dreaw <jezierski.michal13@gmail.com>
2024-06-19 07:08:04 +0200

    Implemented base classes for components

--- decs/src/decs/Component/Component.h
+++ decs/src/decs/Component/Component.h
@@ -1,15 +1,26 @@
 #pragma once
 
+#include "traits.h"
+
 namespace decs
 {
-	class Component
+	class ComponentBase
+	{
+		friend class Component;
+		friend class StableComponent;
+
+	private:
+		virtual ~ComponentBase() = default;
+	};
+
+	class Component : public ComponentBase
 	{
 	public:
 		inline static constexpr bool IsStable = false;
 
 	};
 
-	class StableComponent
+	class StableComponent : public ComponentBase
 	{
 	public:
 		inline static constexpr bool IsStable = true;
