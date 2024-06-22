a0b8bc2f7b5e665bb497469308817394fd2c0af5
Michał <jezierski.michal13@gmail.com>
2024-06-22 22:17:09 +0200

    Removed unnecesary data from component class

--- decs/src/decs/Component/Component.h
+++ decs/src/decs/Component/Component.h
@@ -24,7 +24,6 @@ namespace decs
 	private:
 		bool m_bIsCreatedByContainer = false;
 		bool m_bIsEnabledByECS = false;
-		bool m_bIsDestroyed = false; // if is destroyed cant perform any operation on this component
 	};
 
 }
