05a36d47fe3e4897523348fc68ba81317f098533
Michal Jezierski <jezierski.michal13@gmail.com>
2025-04-16 18:27:18 +0200

    Added tag traits templates

---
+++ decs/src/decs/trait.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <type_traits>
+
+namespace decs
+{
+	template<typename T>
+	struct tag final
+	{
+	};
+
+	template<typename T>
+	struct is_tag final
+	{
+	public:
+		inline static constexpr bool value = false;
+	};
+
+	template<typename T>
+	struct is_tag<tag<T>> final
+	{
+	public:
+		inline static constexpr bool value = true;
+	};
+
+	template<typename T>
+	inline constexpr bool is_tag_v = is_tag<T>::value;
+
+}
